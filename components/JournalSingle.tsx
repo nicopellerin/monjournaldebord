0e7dcce9f70540e775f3313f990eff4ade22f95b
Nico Pellerin <hello@nicopellerin.io>
2020-03-02 18:04:52 -0500

    Add dateInfo prop to date component to display publication date

--- components/JournalSingle.tsx
+++ components/JournalSingle.tsx
@@ -10,146 +10,146 @@ import { DateNow } from './DateNow'
 import { JournalContext } from '../context/JournalProvider'
 
 export const JournalSingle: React.FC = () => {
-    const {
-        selectJournal,
-        selectedJournal,
-        journals,
-        toggleEditing,
-        deleteSelectedJournal,
-    } = useContext(JournalContext)
+  const {
+    selectJournal,
+    selectedJournal,
+    journals,
+    toggleEditing,
+    deleteSelectedJournal,
+  } = useContext(JournalContext)
 
-    useEffect(() => {
-        const prevIdx = Number(selectedJournal?.id) - 1
-        const nextIdx = Number(selectedJournal?.id) + 1
+  useEffect(() => {
+    const prevIdx = Number(selectedJournal?.id) - 1
+    const nextIdx = Number(selectedJournal?.id) + 1
 
-        const prevPublication = e => {
-            if (prevIdx > 0 && e.keyCode === 38) {
-                Router.push(`/journal/[id]`, `/journal/${prevIdx}`, {
-                    shallow: true,
-                })
-                selectJournal(prevIdx)
-            }
-        }
-        document.addEventListener('keydown', prevPublication)
+    const prevPublication = e => {
+      if (prevIdx > 0 && e.keyCode === 38) {
+        Router.push(`/journal/[id]`, `/journal/${prevIdx}`, {
+          shallow: true,
+        })
+        selectJournal(prevIdx)
+      }
+    }
+    document.addEventListener('keydown', prevPublication)
 
-        const nextPublication = e => {
-            if (nextIdx <= journals.length && e.keyCode === 40) {
-                Router.push(`/journal/[id]`, `/journal/${nextIdx}`, {
-                    shallow: true,
-                })
-                selectJournal(nextIdx)
-            }
-        }
+    const nextPublication = e => {
+      if (nextIdx <= journals.length && e.keyCode === 40) {
+        Router.push(`/journal/[id]`, `/journal/${nextIdx}`, {
+          shallow: true,
+        })
+        selectJournal(nextIdx)
+      }
+    }
 
-        document.addEventListener('keydown', nextPublication)
+    document.addEventListener('keydown', nextPublication)
 
-        return () => {
-            document.removeEventListener('keydown', prevPublication)
-            document.removeEventListener('keydown', nextPublication)
-        }
-    }, [selectedJournal])
+    return () => {
+      document.removeEventListener('keydown', prevPublication)
+      document.removeEventListener('keydown', nextPublication)
+    }
+  }, [selectedJournal])
 
-    return (
-        <Wrapper>
-            {journals.length > 0 ? (
-                <>
-                    <Title>{selectedJournal?.title || journals[0].title}</Title>
-                    <DateWrapper>
-                        <FaCalendar style={{ marginRight: 8 }} />
-                        <DateNow />
-                    </DateWrapper>
-                    <Text>{selectedJournal?.text || journals[0].text}</Text>
-                    <ButtonWrapper>
-                        <Link
-                            href={`/journal/edit/[id]`}
-                            as={`/journal/edit/${selectedJournal?.id}`}
-                        >
-                            <ButtonEdit
-                                onClick={toggleEditing}
-                                whileHover={{ scale: 1.02 }}
-                                whileTap={{ scale: 0.98 }}
-                            >
-                                <FaEdit style={{ marginRight: 5 }} />
-                                Editer
-                            </ButtonEdit>
-                        </Link>
-                        <ButtonDelete
-                            whileHover={{ scale: 1.02 }}
-                            whileTap={{ scale: 0.98 }}
-                            onClick={() => {
-                                deleteSelectedJournal(selectedJournal.id)
-                                Router.push(
-                                    `/journal/${journals[0].id}`,
-                                    `/journal/${journals[0].id}`,
-                                    { shallow: true }
-                                )
-                            }}
-                        >
-                            <FaTimes style={{ marginRight: 5 }} />
-                            Supprimer
-                        </ButtonDelete>
-                    </ButtonWrapper>
-                </>
-            ) : (
-                <div>No journals</div>
-            )}
-        </Wrapper>
-    )
+  return (
+    <Wrapper>
+      {journals.length > 0 ? (
+        <>
+          <Title>{selectedJournal?.title || journals[0].title}</Title>
+          <DateWrapper>
+            <FaCalendar style={{ marginRight: 8 }} />
+            <DateNow dateInfo={Date.parse(selectedJournal?.createdAt)} />
+          </DateWrapper>
+          <Text>{selectedJournal?.text || journals[0].text}</Text>
+          <ButtonWrapper>
+            <Link
+              href={`/journal/edit/[id]`}
+              as={`/journal/edit/${selectedJournal?.id}`}
+            >
+              <ButtonEdit
+                onClick={toggleEditing}
+                whileHover={{ scale: 1.02 }}
+                whileTap={{ scale: 0.98 }}
+              >
+                <FaEdit style={{ marginRight: 5 }} />
+                Editer
+              </ButtonEdit>
+            </Link>
+            <ButtonDelete
+              whileHover={{ scale: 1.02 }}
+              whileTap={{ scale: 0.98 }}
+              onClick={() => {
+                deleteSelectedJournal(selectedJournal.id)
+                Router.push(
+                  `/journal/${journals[0].id}`,
+                  `/journal/${journals[0].id}`,
+                  { shallow: true }
+                )
+              }}
+            >
+              <FaTimes style={{ marginRight: 5 }} />
+              Supprimer
+            </ButtonDelete>
+          </ButtonWrapper>
+        </>
+      ) : (
+        <div>No journals</div>
+      )}
+    </Wrapper>
+  )
 }
 
 // Styles
 const Wrapper = styled.div`
-    max-width: 80rem;
+  max-width: 80rem;
 `
 
 const Title = styled.h2`
-    font-size: 6rem;
-    word-break: break-all;
-    margin-bottom: 3rem;
+  font-size: 6rem;
+  word-break: break-all;
+  margin-bottom: 3rem;
 `
 
 const Text = styled.p`
-    font-size: 1.6rem;
-    line-height: 1.5em;
-    margin-bottom: 3rem;
+  font-size: 1.6rem;
+  line-height: 1.5em;
+  margin-bottom: 3rem;
 `
 
 const DateWrapper = styled.div`
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 `
 
 const ButtonWrapper = styled.div`
-    display: flex;
-    justify-content: flex-end;
+  display: flex;
+  justify-content: flex-end;
 `
 
 const ButtonEdit = styled(motion.button)`
-    border: none;
-    padding: 1em 1.5em;
-    background: whitesmoke;
-    color: #333;
-    text-transform: uppercase;
-    border-radius: 5px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    font-size: 1.4rem;
-    margin-right: 2rem;
+  border: none;
+  padding: 1em 1.5em;
+  background: whitesmoke;
+  color: #333;
+  text-transform: uppercase;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  font-size: 1.4rem;
+  margin-right: 2rem;
 `
 
 const ButtonDelete = styled(motion.button)`
-    border: none;
-    padding: 1em 1.5em;
-    background: whitesmoke;
-    color: crimson;
-    text-transform: uppercase;
-    border-radius: 5px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    font-size: 1.4rem;
-    margin-right: 2rem;
+  border: none;
+  padding: 1em 1.5em;
+  background: whitesmoke;
+  color: crimson;
+  text-transform: uppercase;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  font-size: 1.4rem;
+  margin-right: 2rem;
 `
