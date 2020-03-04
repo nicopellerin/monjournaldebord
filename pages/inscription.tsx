64749625bbd7dae4c062a46af5b0522417709b10
Nico Pellerin <hello@nicopellerin.io>
2020-03-04 13:22:14 -0500

    A

--- pages/inscription.tsx
+++ pages/inscription.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export const Inscription: React.FC = () => {
+const Inscription: React.FC = () => {
   return (
     <div>
       <h1>S'inscrire</h1>
     </div>
   )
 }
+
+export default Inscription
