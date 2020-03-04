76d8b6a7dd67a3a4dd264f35e31502b7f6eb3506
Nico Pellerin <hello@nicopellerin.io>
2020-03-04 13:15:56 -0500

    Fix content wrapper height

---
+++ pages/connexion.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Connexion: React.FC = () => {
+  return (
+    <div>
+      <h1>Se connecter</h1>
+    </div>
+  )
+}
---
+++ pages/inscription.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Inscription: React.FC = () => {
+  return (
+    <div>
+      <h1>S'inscrire</h1>
+    </div>
+  )
+}
